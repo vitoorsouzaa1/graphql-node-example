aff455bde5618ad2238c6dbf54baf0ba46f297e7
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2022-05-31 22:48:24 -0300

    Added Mutations for newProducts and productReviews

--- products/products.model.js
+++ products/products.model.js
@@ -3,11 +3,13 @@ const products = [
     id: 'redshoe',
     description: 'Red Shoe',
     price: 43.99,
+    reviews: [],
   },
   {
     id: 'bluejean',
     description: 'Blue Jeans',
     price: 55.99,
+    reviews: [],
   },
 ];
 
@@ -27,8 +29,35 @@ function getProductById(id) {
   });
 }
 
+function addNewProduct(id, description, price) {
+  const newProduct = {
+    id,
+    price,
+    description,
+    reviews: [],
+  };
+
+  products.push(newProduct);
+  return newProduct;
+}
+
+function addNewProductReview(id, rating, comment) {
+  const matchedProduct = getProductById(id);
+
+  if (matchedProduct) {
+    const newProductReview = {
+      rating,
+      comment,
+    };
+    matchedProduct.reviews.push(newProductReview);
+    return newProductReview;
+  }
+}
+
 module.exports = {
   getAllProducts,
   getProductsByPrice,
   getProductById,
+  addNewProduct,
+  addNewProductReview,
 };
--- products/products.resolvers.js
+++ products/products.resolvers.js
@@ -13,4 +13,16 @@ module.exports = {
       return productsModel.getProductById(args.id);
     },
   },
+  Mutation: {
+    addNewProduct: (_, args) => {
+      return productsModel.addNewProduct(args.id, args.description, args.price);
+    },
+    addNewProductReview: (_, args) => {
+      return productsModel.addNewProductReview(
+        args.id,
+        args.rating,
+        args.comment
+      );
+    },
+  },
 };
